1e085baebd48474a0f6c9f235472a4ba6fd8da12
Ihor <157710697+Firkax69@users.noreply.github.com>
2024-04-19 23:40:42 +0100

    feature: style CSS start JS selectors

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
 }
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function () {
+  let newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = "block";
+})
