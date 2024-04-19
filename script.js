db805bb3ed1294701498a7f37395b4fe8e4769f2
Ihor <157710697+Firkax69@users.noreply.github.com>
2024-04-20 00:30:20 +0100

    feature: add response to remove and create book button

--- script.js
+++ script.js
@@ -7,16 +7,51 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+function render() {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.setAttribute("class", "book-card")
+    bookEl.innerHTML = `
+      <div class="card-header">
+        <h3 class="title">${book.title}</h3>
+        <h5 class="author">${book.author}</h5>
+      </div>
+      <div class="card-body">
+          <p>${book.pages} pages</p>
+          <p class="read-status">${book.read ? "Read" : "Not read yet"}</p>
+          <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+      </div>
+    `;
+    libraryEl.appendChild(bookEl);
+  }
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  render();
+}
+
 function addBookToLibrary() {
   // do stuff here
   let title = document.querySelector("#title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
   let read = document.getElementById("read").checked;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  render(); /* should display the items of myLibrary array in HTML */
 }
 
 let newBookbtn = document.querySelector("#new-book-btn");
 newBookbtn.addEventListener("click", function () {
   let newBookForm = document.querySelector("#new-book-form");
   newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  addBookToLibrary();
 })
